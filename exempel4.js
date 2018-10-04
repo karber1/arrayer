1c317a11259ed470ebcbda8989c04a8a999f6420
karber1 <karl.berglof@hotmail.com>
2018-10-04 13:23:52 +0200

    arrayer

---
+++ exempel4.js
@@ -0,0 +1,14 @@
+//let kurser = ["introduktion", "javascript1", "javascript2", "HTML och CSS", "Vektorverktyg", "LIA"];
+//console.log(kurser.sort());
+//console.log("antal kurser:" + kurser.length);
+
+let customers = ["kund1", "kund2", "kund3"];
+let unknown = customers[customers.length -1];
+customers[0] = "kund711";
+console.log(unknown + customers[0]);
+
+
+
+
+
+
